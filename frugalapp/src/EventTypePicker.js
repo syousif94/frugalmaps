62b30471991128cf53bc82dd6ecd26f60f8cefaf
Sammy Yousif <syousif@utexas.edu>
2018-10-08 14:01:49 -0500

    implementing event type picker

--- frugalapp/src/EventTypePicker.js
+++ frugalapp/src/EventTypePicker.js
@@ -1,4 +1,28 @@
 import React, { Component } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 
-class EventTypePicker extends Component {}
+class EventTypePicker extends Component {
+  static types = ["Happy Hour", "Brunch", "Daily Special"];
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {EventTypePicker.types.map((text, index) => {
+          return (
+            <View style={styles.type} key={index}>
+              <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btnText}>{text}</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 2
+  }
+});
