55a1043f0809b7e773e97808babc84edc071cf59
Sammy Yousif <syousif@utexas.edu>
2018-10-16 02:15:04 -0500

    configure ads for dev mode

--- frugalapp/App.js
+++ frugalapp/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Provider } from "react-redux";
 import "rxjs";
+import { FacebookAds } from "expo";
 import Navigator from "./src/Navigator";
 import store from "./src/store";
+import { DEV } from "./src/Constants";
+
+if (DEV) {
+  FacebookAds.AdSettings.addTestDevice(
+    FacebookAds.AdSettings.currentDeviceHash
+  );
+}
 
 export default () => (
   <Provider store={store}>
