7430ea474b5e8634fcce8d5a773b30dab6d8f88a
Sammy Yousif <syousif@utexas.edu>
2018-11-07 03:27:13 -0600

    better handling of weird locations

--- functions/queryEvents.js
+++ functions/queryEvents.js
@@ -1,6 +1,7 @@
 const elastic = require("./elastic");
 const event = require("./schema/event");
 const servicesApi = require("./google");
+const turf = require("@turf/turf");
 
 function queryEvents(req, res) {
   const { bounds: queryBounds, lat, lng } = req.body;
@@ -46,8 +47,7 @@ function queryEvents(req, res) {
     const reverseGeocode = new Promise((resolve, reject) => {
       servicesApi.reverseGeocode(
         {
-          latlng: [lat, lng],
-          result_type: ["locality"]
+          latlng: [lat, lng]
         },
         (err, response) => {
           if (err) {
@@ -55,37 +55,97 @@ function queryEvents(req, res) {
             return;
           }
 
-          resolve(response.json.results[0]);
+          resolve(response.json.results);
         }
       );
     });
-    query = reverseGeocode.then(geocode => {
-      text = geocode.formatted_address;
-      bounds = geocode.geometry.bounds;
+    query = reverseGeocode.then(results => {
+      let geocode = results.find(location => {
+        return location.types.indexOf("locality") > -1;
+      });
 
-      const { northeast, southwest } = bounds;
+      if (!geocode) {
+        geocode = results.find(location => {
+          return location.types.indexOf("neighborhood") > -1;
+        });
+      } else {
+        text = geocode.formatted_address;
+        bounds = geocode.geometry.bounds;
+      }
 
-      const coordinates = {
-        top_left: [southwest.lng, northeast.lat],
-        bottom_right: [northeast.lng, southwest.lat]
-      };
+      if (!geocode) {
+        geocode = results.find(location => {
+          return location.types.indexOf("street_address") > -1;
+        });
+      }
+
+      if (geocode && !text) {
+        const city = geocode.address_components.find(
+          component => component.types.indexOf("locality") > -1
+        );
+
+        let state = geocode.address_components.find(
+          component =>
+            component.types.indexOf("administrative_area_level_1") > -1
+        );
+
+        if (!state) {
+          state = geocode.address_components.find(
+            component => component.types.indexOf("country") > -1
+          );
+        }
+
+        text = `${city.long_name}, ${state.long_name}`;
+      }
 
       body.query = {
         bool: {
           must: [{ match_all: {} }],
           filter: {
-            geo_bounding_box: {
-              coordinates
+            geo_distance: {
+              distance: "20mi",
+              coordinates: [lng, lat]
             }
           }
         }
       };
 
-      return elastic.search({
-        index: event.index,
-        type: event.type,
-        body
-      });
+      return elastic
+        .search({
+          index: event.index,
+          type: event.type,
+          body
+        })
+        .then(results => {
+          if (!bounds) {
+            const envelope = turf.bbox(
+              turf.buffer(
+                turf.envelope(
+                  turf.featureCollection([
+                    ...results.hits.hits.map(doc =>
+                      turf.point(doc._source.coordinates)
+                    ),
+                    turf.point([lng, lat])
+                  ])
+                ),
+                0.5,
+                { units: "miles" }
+              )
+            );
+
+            bounds = {
+              southwest: {
+                lat: envelope[1],
+                lng: envelope[0]
+              },
+              northeast: {
+                lat: envelope[3],
+                lng: envelope[2]
+              }
+            };
+          }
+          return results;
+        });
     });
   } else {
     text = "Recently Added";
@@ -101,9 +161,9 @@ function queryEvents(req, res) {
       const hits = results.hits.hits;
 
       res.send({
-        bounds,
         text,
-        hits
+        hits,
+        bounds
       });
     })
     .catch(error => {
