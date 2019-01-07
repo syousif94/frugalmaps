6e436d339b7e696488a1ddbacd233b1ba84a0eba
Sammy Yousif <syousif@utexas.edu>
2019-01-07 16:33:33 -0600

    action buttons

--- frugalapp/src/InfoActions.js
+++ frugalapp/src/InfoActions.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
-import { FontAwesome, MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 
 export default () => {
   return (
@@ -8,73 +8,30 @@ export default () => {
       {["Call", "Web", "Hours", "Go", "Share"].map(text => {
         let icon = null;
         switch (text) {
-          // case "Map":
-          //   icon = (
-          //     <FontAwesome
-          //       style={styles.icon}
-          //       name="map-marker"
-          //       size={18}
-          //       color="#fff"
-          //     />
-          //   );
-          //   break;
           case "Call":
-            icon = (
-              <Ionicons
-                style={styles.icon}
-                name="ios-call"
-                size={18}
-                color="#fff"
-              />
-            );
+            icon = <Ionicons name="ios-call" size={18} color="#fff" />;
             break;
           case "Web":
-            icon = (
-              <MaterialIcons
-                style={styles.icon}
-                name="web-asset"
-                size={18}
-                color="#fff"
-              />
-            );
+            icon = <MaterialIcons name="web-asset" size={18} color="#fff" />;
             break;
           case "Hours":
-            icon = (
-              <Ionicons
-                style={styles.icon}
-                name="ios-time"
-                size={15}
-                color="#fff"
-              />
-            );
+            icon = <Ionicons name="ios-time" size={15} color="#fff" />;
             break;
           case "Go":
-            icon = (
-              <MaterialIcons
-                style={styles.icon}
-                name="directions"
-                size={17}
-                color="#fff"
-              />
-            );
+            icon = <MaterialIcons name="directions" size={17} color="#fff" />;
             break;
           case "Share":
-            icon = (
-              <Ionicons
-                style={styles.icon}
-                name="ios-share-alt"
-                size={18}
-                color="#fff"
-              />
-            );
+            icon = <Ionicons name="ios-share-alt" size={18} color="#fff" />;
             break;
           default:
             break;
         }
         return (
           <View key={text} style={styles.action}>
-            {icon}
-            <Text style={styles.actionText}>{text}</Text>
+            <TouchableOpacity style={styles.btn}>
+              <View style={styles.icon}>{icon}</View>
+              <Text style={styles.actionText}>{text}</Text>
+            </TouchableOpacity>
           </View>
         );
       })}
@@ -94,16 +51,23 @@ const styles = StyleSheet.create({
   action: {
     marginHorizontal: 3,
     flex: 1,
-    paddingVertical: 6,
     backgroundColor: "rgba(0,0,0,0.5)",
-    borderRadius: 4,
-    justifyContent: "flex-end",
+    borderRadius: 4
+  },
+  btn: {
+    flex: 1,
+    paddingVertical: 6,
+    alignItems: "center"
+  },
+  icon: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center"
   },
   actionText: {
     marginTop: 1,
     fontSize: 7,
     fontWeight: "500",
-    color: "#fff"
+    color: "#e0e0e0"
   }
 });
