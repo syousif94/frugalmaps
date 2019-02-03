9575241dddc07e963ee2f1e7fa4ec7c22eea486c
Sammy Yousif <syousif@utexas.edu>
2019-02-03 17:20:00 -0600

    fixed deleting events

--- functions/removeFromS3.js
+++ functions/removeFromS3.js
@@ -2,27 +2,14 @@ const AWS = require("./aws");
 const s3 = new AWS.S3();
 
 module.exports = async function remove(key) {
-  const copyParams = {
+  const params = {
     Bucket: "buncha",
-    CopySource: `backup/${key}.json`,
-    Key: `deleted/${key}.json`
-  };
-  const deleteParams = {
-    Bucket: "buncha",
-    Key: `backup/${key}.json`
+    Key: `deleted/${key}.txt`,
+    Body: ""
   };
   try {
     await new Promise((resolve, reject) => {
-      s3.copyObject(copyParams, err => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
-      });
-    });
-    await new Promise((resolve, reject) => {
-      s3.deleteObject(deleteParams, err => {
+      s3.putObject(params, err => {
         if (err) {
           reject(err);
           return;
